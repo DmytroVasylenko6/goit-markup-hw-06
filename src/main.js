8e7631000e902f72d5981e866662cda0d33c2c7a
DmytroVasylenko6 <tredstoun651@ukr.net>
2024-07-24 15:39:23 +0300

    add changes

---
+++ src/main.js
@@ -0,0 +1,31 @@
+import 'modern-normalize';
+
+const modalRefs = {
+  openModalBtn: document.querySelector('[data-modal-open]'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  modal: document.querySelector('[data-modal]'),
+};
+
+const toggleModal = () => {
+  modalRefs.modal.classList.toggle('is-open');
+};
+
+modalRefs.openModalBtn.onclick = toggleModal;
+modalRefs.closeModalBtn.onclick = toggleModal;
+
+const mobileMenuRefs = {
+  mobileMenu: document.querySelector('[data-menu]'),
+  openMenuBtn: document.querySelector('[data-menu-button]'),
+  closeMenuBtn: document.querySelector('[data-menu-close]'),
+};
+
+const toggleMenu = () => {
+  const isMenuOpen = mobileMenuRefs.openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+
+  mobileMenuRefs.mobileMenu.classList.toggle('is-open');
+  mobileMenuRefs.mobileMenu.setAttribute('aria-hidden', isMenuOpen);
+  mobileMenuRefs.openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+};
+
+mobileMenuRefs.openMenuBtn.addEventListener('click', toggleMenu);
+mobileMenuRefs.closeMenuBtn.addEventListener('click', toggleMenu);
